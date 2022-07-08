6c84dace9ac049fa4783f0d0b72c58a4cce79d39
TheH3ROo <rab.peter97@gmail.com>
2022-07-08 18:10:36 +0200

    continued the gohtml discovery

--- htmlGame/main.go
+++ htmlGame/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tmpl *template.Template
+
+type Game struct {
+	Players []Player
+}
+
+type Player struct {
+	Name   string
+	Hurt   bool
+	Health int
+	Weapon string
+}
+
+type PageData struct {
+	Title string
+	Game  Game
+}
+
+func game(w http.ResponseWriter, r *http.Request) {
+	data := PageData{
+		Title: "Testing htmlGame",
+		Game: Game{
+			[]Player{
+				{Name: "John", Hurt: false, Health: 100, Weapon: " "},
+				{Name: "Bill", Hurt: true, Health: 90, Weapon: " "},
+			},
+		},
+	}
+	tmpl.Execute(w, data)
+}
 
 func main() {
 	fmt.Println("Init Html game")
+
+	mux := http.NewServeMux()
+	tmpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+
+	fs := http.FileServer(http.Dir("/.static"))
+	mux.Handle("/static", http.StripPrefix("/static/", fs))
+
+	mux.HandleFunc("/game", game)
+
+	log.Fatal(http.ListenAndServe(":9090", mux))
 }
